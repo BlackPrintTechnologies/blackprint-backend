a315d57ad18ead6b3a686de90ee659c0056103a4
Avinash Vishwakarma <avinash@geoiq.io>
2025-03-24 23:09:12 +0530

    Improvement related to layer

--- utils/dbUtils.py
+++ utils/dbUtils.py
@@ -43,39 +43,6 @@ class Database:
             self.connection.close()
             self.connection = None
 
-class Database:
-    _instance = None
-    _lock = Lock()
-
-    def __new__(cls):
-        with cls._lock:
-            if cls._instance is None:
-                cls._instance = super(Database, cls).__new__(cls)
-                cls._instance._initialize()
-        return cls._instance
-
-    def _initialize(self):
-        self.db_config = {
-            'host': config['DB_HOST'],
-            'database': config['DB_NAME'],
-            'user': config['DB_USERNAME'],
-            'password': config['DB_PASSWORD'],
-            'port': config['DB_PORT']
-        }
-        self.connection = None
-
-    def connect(self, db_name=None):
-        if self.connection is None:
-            if db_name:
-                self.db_config['database'] = db_name
-            self.connection = psycopg2.connect(**self.db_config)
-        return self.connection
-
-    def disconnect(self):
-        if self.connection is not None:
-            self.connection.close()
-            self.connection = None
-
 class RedshiftDatabase:
     _instance = None
     _lock = Lock()
