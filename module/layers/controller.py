7c7614ebcb7447b140f8134d8acb222d60fcb97e
sumeet <>
2025-06-13 20:18:13 +0530

    add  category filter on brands api

--- module/layers/controller.py
+++ module/layers/controller.py
@@ -111,7 +111,7 @@ class BrandController:
         return query
     
     # @cache_response(prefix='brands',expiration=3600)
-    def get_brands(self, radius, fid): 
+    def get_brands(self, radius, fid, category=None): 
         connection = None
         cursor = None
         resp = None
@@ -130,6 +130,9 @@ class BrandController:
             for result in res:
                 result['icon_url'] = IconMapper.get_icon_url(result['category_1'])
                 enhanced_results.append(result)
+            #Filter by category
+            if category:
+                enhanced_results = [result for result in enhanced_results if result['category_1'] == category]
             print("enhanced_results=====>", enhanced_results)
             resp =  Response.success(data={"response": enhanced_results}) #
         except Exception as e :
