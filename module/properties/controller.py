e971c6aade609e662f28a91919c0c613246b7cd6
Avinash Vishwakarma <avinash@geoiq.io>
2025-07-10 22:48:53 +0530

    Improvement related to test

--- module/properties/controller.py
+++ module/properties/controller.py
@@ -1359,7 +1359,7 @@ class PropertyController:
             allowed_keys = {"neighborhood", "zip_code", "municipality_nm"}
             # If both municipality_nm and (search_key_type + search_value) are provided, filter within municipality_nm
             if municipality_nm and search_key_type in {"neighborhood", "zip_code"} and search_value:
-                query = f"SELECT DISTINCT id_municipality, {search_key_type} FROM presentation.dim_municipality WHERE municipality_nm ILIKE %s AND {search_key_type} ILIKE %s"
+                query = f"SELECT DISTINCT id_municipality, {search_key_type} FROM presentation.dim_municipality WHERE municipality_nm ILIKE %s AND {search_key_type} ILIKE %s limit 50"
                 cursor.execute(query, (f"%{municipality_nm}%", f"%{search_value}%"))
                 results = cursor.fetchall()
                 items = [{"id": row["id_municipality"], "name": row[search_key_type]} for row in results]
@@ -1374,7 +1374,7 @@ class PropertyController:
                     message=message
                 )
             elif  search_key_type == "municipality_nm" and search_value:
-                query = f"SELECT DISTINCT id_municipality, {search_key_type} FROM presentation.dim_municipality WHERE {search_key_type} ILIKE %s"
+                query = f"SELECT DISTINCT id_municipality, {search_key_type} FROM presentation.dim_municipality WHERE {search_key_type} ILIKE %s limit 50"
                 cursor.execute(query, (f"%{search_value}%",))
                 results = cursor.fetchall()
                 items = [{"id": row["id_municipality"], "name": row[search_key_type]} for row in results]
