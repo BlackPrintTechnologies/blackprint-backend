55e22d2d256b33a605fe7e38a8a0f48e06a9cf61
Avinash vishwakarma <avinashvishwakarma@Avinashs-MacBook-Air.local>
2025-08-31 18:19:06 +0530

    brand layers deployed

--- module/layers/routes.py
+++ module/layers/routes.py
@@ -127,7 +127,7 @@ class Brands(Resource):
     create_parser.add_argument('fid', type=str, required=False, help='User ID is required')
     create_parser.add_argument('category', type=str, required=False, help='Category is required')
     create_parser.add_argument('config_city', type=str, required=False, default='mexico', help='City is required')
-    create_parser.add_argument('brand_name', type=str, required=False, help='Brand name is required')
+    create_parser.add_argument('brand_names', type=str, required=False, help='Brand names is required')
 
     def post(self):
         logger.info("Received request to fetch brands.")
@@ -136,10 +136,10 @@ class Brands(Resource):
         fid = data.get('fid')
         radius = data.get('radius')
         category = data.get('category')
-        brand_name = data.get('brand_name')
-        logger.debug(f"Parsed input: fid={fid}, radius={radius} ,category={category}, brand_name={brand_name}")
+        brand_names = data.get('brand_names')
+        logger.debug(f"Parsed input: fid={fid}, radius={radius} ,category={category}, brand_names={brand_names}")
 
-        response = brand_controller.get_brands(radius, fid, category, brand_name, city=data.get('config_city'))
+        response = brand_controller.get_brands(radius, fid, category, brand_names, city=data.get('config_city'))
         logger.info(f"Successfully retrieved brands for fid={fid}, radius={radius}")
         
         return response
