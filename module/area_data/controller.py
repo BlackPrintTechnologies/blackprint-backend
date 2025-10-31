efbed0428130780ec4fa5558899cf939f1c2dc89
Avinash vishwakarma <05avinashvish@gmail.com>
2025-10-31 21:53:45 +0530

    test

---
+++ module/area_data/controller.py
@@ -0,0 +1,78 @@
+"""Base classes for area data operations."""
+
+import logging
+from . import query
+from psycopg2.extras import RealDictCursor
+from utils.dbUtils import Database, RedshiftDatabase
+from abc import ABC
+logger = logging.getLogger(__name__)
+
+
+class AbstractAreaData(ABC):
+    """Base class for area data operations."""
+    
+    def __init__(self):
+        """Initialize the AbstractAreaData class."""
+        self.db = Database()
+        self.redshift_db = RedshiftDatabase()
+        pass
+    
+    def get_data(self, boundary):
+        """Get data for the selected area."""
+        raise NotImplementedError("Subclasses must implement get_data")
+
+class DemographicsAreaData(AbstractAreaData) :
+    """Class for demographic data operations."""
+    
+    def __init__(self):
+        """Initialize the Demographics class."""
+        super().__init__()
+    
+    def get_data(self, boundary):
+        """Get demographic data for the selected area (table: 'block' or 'locality')."""
+        q = query.build_demographics_query(boundary)
+        redshift_connection = self.redshift_db.connect()
+        cursor = redshift_connection.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(q)
+        data = cursor.fetchall()
+        cursor.close()
+        redshift_connection.close()
+        return data 
+
+
+class SocioeconomicAreaData(AbstractAreaData) :
+    """Class for socio-economic data operations."""
+    
+    def __init__(self):
+        """Initialize the Socioeconomic class."""
+        super().__init__()
+    
+    def get_data(self, boundary):
+        """Get socio-economic data for the selected area (table: 'ageb' or 'locality')."""
+        q = query.build_socioeconomic_query_controller(boundary)
+        redshift_connection = self.redshift_db.connect()
+        cursor = redshift_connection.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(q)
+        data = cursor.fetchall()
+        cursor.close()
+        redshift_connection.close()
+        return data 
+
+
+class PoisAreaData(AbstractAreaData) :
+    """Class for POIs data operations."""
+    
+    def __init__(self):
+        """Initialize the Pois class."""
+        super().__init__()
+    
+    def get_data(self, boundary):
+        """Get POIs data for the selected area."""
+        q = query.build_pois_query(boundary)
+        redshift_connection = self.redshift_db.connect()
+        cursor = redshift_connection.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(q)
+        data = cursor.fetchall()
+        cursor.close()
+        redshift_connection.close()
+        return data 
--- module/area_data/query.py
+++ module/area_data/query.py
@@ -68,3 +68,34 @@ def build_socioeconomic_query(lng, lat, radius, geometry_column='geometry_coords
     
     return query
 
+
+def build_pois_query(catchment):
+    """Build query to get POIs data for the selected catchment."""
+    
+    query = f"""
+        WITH geom_input AS (
+            SELECT ST_GeomFromText('{catchment}', 4326) AS geom
+        ),
+        h3_values AS (
+            SELECT H3_Polyfill(geom, 10) AS h3_indexes FROM geom_input
+        ),
+        h3_index AS (
+            SELECT o AS h3_value
+            FROM h3_values i, i.h3_indexes o
+        )
+        SELECT CASE WHEN chain_id != 'None' THEN chain_id ELSE NULL END AS brand,
+               name AS names_pri,
+               geometry_wkt,
+               main_category,
+               sub_category,
+               sub_sub_category,
+               business_category,
+               open_closed_status,
+               popularity_score,
+               average_stars,
+               number_of_reviews,
+               sentiment_score
+        FROM blackprint_db_prd.presentation.dim_pois_qro a
+        INNER JOIN h3_index b ON a.h3_value = b.h3_value
+    """
+    return query
